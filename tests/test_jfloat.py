15d0ad4539f5ada33c72a56e187e1dc206cc7b6b
Yan Victor <yanvictorm79@gmail.com>
2026-06-19 14:50:55 -0400

    test: adiciona testes de aritmetica de JFloat - refs #41

--- tests/test_jfloat.py
+++ tests/test_jfloat.py
@@ -16,8 +16,19 @@ Um Pull Request NÃO pode conter mais do que 7 casos de testes implementados.
 Faça PRs curtos e frequentes!
 Um commit não pode conter mais de 3 métodos de teste.
 """
-# from javalang.jfloat import JFloat
+import math
+from javalang.jfloat import JFloat
 
 def test_jfloat_is_nan():
     # TODO: Implementar teste com casos de borda do IEEE 754
     pass
+
+class TestJFloatAritmetica:
+    def test_max(self):
+        assert JFloat.max(1.0, 2.0) == 2.0
+
+    def test_min(self):
+        assert JFloat.min(1.0, 2.0) == 1.0
+
+    def test_sum(self):
+        assert math.isclose(JFloat.sum(1.5, 2.5), 4.0, rel_tol=1e-6)
