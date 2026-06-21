e7aa0de20cc7d95564ffed7d778ea52f67455039
Yan Victor <yanvictorm79@gmail.com>
2026-06-21 14:07:28 -0400

    test: adiciona testes de split e intern de JString - refs #60

--- tests/test_jstring.py
+++ tests/test_jstring.py
@@ -70,3 +70,19 @@ class TestJStringMatchesEReplace:
     def test_replace_all(self):
         s = JString("aaa")
         assert s.replaceAll("a", "b").toCharArray() == list("bbb")
+
+class TestJStringSplit:
+    def test_split_simples(self):
+        s = JString("a,b,c")
+        partes = s.split(",")
+        assert [p.toCharArray() for p in partes] == [['a'], ['b'], ['c']]
+
+    def test_split_com_limit(self):
+        s = JString("a,b,c")
+        partes = s.split(",", 2)
+        assert len(partes) == 2
+
+    def test_intern_mesmo_valor(self):
+        a = JString("abc")
+        b = JString("abc")
+        assert a.intern() == b.intern()
